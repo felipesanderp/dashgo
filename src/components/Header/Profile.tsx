7a4b7b184919519417abdf01d867aed9d2f4493f
Felipe Sander <felipe.panisson@hotmail.com>
2021-12-28 13:57:05 -0300

    make header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,19 @@
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
+interface ProfileProps {
+  showProfileData?: boolean;
+}
 
-export function Profile() {
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Felipe Sander</Text>
-        <Text color="gray.300" fontSize="small">
-          felipe@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Felipe Sander</Text>
+          <Text color="gray.300" fontSize="small">
+            felipe@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Felipe Sander" />
     </Flex>
     )
