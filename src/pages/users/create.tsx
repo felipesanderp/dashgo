d29ea7a8fb8a424ebd92159927a519a22ca41ff3
Felipe Sander <felipe.panisson@hotmail.com>
2021-12-29 08:39:41 -0300

    make navlink functional

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -3,6 +3,7 @@ import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from 
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Input } from "../../components/Form/Input";
+import Link from "next/link";
 
 export default function CreatUser() {
   return (
@@ -31,7 +32,9 @@ export default function CreatUser() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
